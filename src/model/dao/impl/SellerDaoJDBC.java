faae875dee320cf563025e2fc77816a6eb6162d6
Fernando Barbosa Santos <muribinhalive@gmail.com>
2023-10-15 17:03:49 -0300

    findAll implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -99,8 +99,48 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public List<Seller> findAll() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
 		
-		return null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+"FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.Id "
+					+"ORDER BY Name"
+					);
+					
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();//para controlar a NÃO repetição do departamento na hora de salvar na lista, sem isso ou IF's normais(algo que controle) toda vez que salva na lista um seller, cria um novo departamento repetido
+			
+			while(rs.next()) //wwhile pois o resultado vai ser mais de um ocorrencia
+				{
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					 dep = instantiateDepartment(rs);
+					 map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs,dep);
+				list.add(obj);
+			}
+			
+			return list;
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 	}
 
 	@Override
